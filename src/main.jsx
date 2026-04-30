df30613a9c0be20767087ceb506867fb917459b9
Nisar-baloch <nisarbaloshi786@gmail.com.com>
2026-04-30 14:33:57 +0500

    commit react redux project

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Users from './pages/Users.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/users' element={<Users />} />
+      
+    </Routes>
+  </BrowserRouter>
+
+
+)
+
+
+
